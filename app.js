b567c9a6cac7468650a8950b1b9b6359c9a98e59
Senuri Wijenayake <swijenayake@4180L-143992-M.local>
2018-12-19 00:58:26 +1100

    File structure changed

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var routes = require("./routes/routes.js");
 
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
