84b54b63583f1f7614bfe6abc99658e4f771db40
Senuri Wijenayake <swijenayake@4180L-143992-M.local>
2018-12-19 21:27:50 +1100

    Handling CORS

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var routes = require("./routes/routes.js");
 var app = express();
 var server = require('http').Server(app);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 routes(app);
