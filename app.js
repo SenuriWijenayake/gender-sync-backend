569cbe3abee0b7c6aade33c840f2742bae5e99e7
Senuri Wijenayake <swijenayake@4180L-143992-M.local>
2019-05-26 21:45:06 +1000

    users kept in track at the socket

--- app.js
+++ app.js
@@ -12,16 +12,29 @@ app.use(function(req, res, next) {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 routes(app);
 
 server.listen(process.env.PORT || 5000);
 
+//Chat room code
 const io = require("socket.io")(server);
+var users = [];
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('printMe', (data) => {
-    console.log('This is' + data.name);
+  socket.on('new_connection', (data) => {
+    console.log("Connected user : " + socket.id, data.username);
+    users.push({"id" : socket.id, "username" : data.username});
+    console.log(users);
+  });
+
+  socket.on('new_message', (data) => {
+    io.sockets.emit('new_message', {
+      'message': data.message,
+      'username': data.username
+    });
   });
 });
