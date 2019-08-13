d653c2eee3d3532701bf7d5977bf036b5f12b22d
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2019-08-13 21:27:09 +1000

    fixed chat box disbling

--- app.js
+++ app.js
@@ -86,6 +86,15 @@ io.on('connection', (socket) => {
     });
   });
 
+  socket.on('time_up', (data) => {
+    io.sockets.emit('time_up', {
+      'message': data.message,
+      'username': data.username,
+      'avatar' : data.avatar,
+      'realUser' : (data.realUser == true) ? true : false
+    });
+  });
+
   socket.on('new_question', (data) => {
     io.sockets.emit('new_question', {
       'message': data.message,
